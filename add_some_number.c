a08f4dc49936fd4f78274a69c3b57af9376002b3
Seehwan Yoo <seehwan.yoo@dankook.ac.kr>
2020-03-19 23:53:41 +0900

    add_some_number.c sample program
    sample program to compile
    
    To compile:
    	gcc ./add_some_number.c -o a.out
    To run:
    	./a.out

---
+++ add_some_number.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#define LONG_MIN 0
+#define LONG_MAX 0xFFFFFFFF
+
+int sum = 0;
+int main(int argc, char * argv[])
+{
+  int number = 0;
+  int i = 0;
+  if (argc == 2) {
+    number = atoi(argv[1]);
+    if ((errno == ERANGE && 
+		(number == LONG_MAX ||  number == LONG_MIN))
+		|| (errno != 0 && number == 0)) {
+			perror("strtol");
+			exit(EXIT_FAILURE);
+    }
+  } else {
+    number = 100;
+  }
+  for (i = 0 ; i <= number ; i++) {
+    sum += i;
+  }
+  printf("sum from 0 to %d: %d (0x%x)\n", 
+               number, sum, sum);
+  return 0;
+}
+
